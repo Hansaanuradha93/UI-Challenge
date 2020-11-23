8b390b8bbaff81de1d854a0d17d17ed0b55858a1
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-11-23 20:41:10 +0530

    Navigation bar added to HomeVC

--- UI Challenge/Scenes/Home/HomeVC.swift
+++ UI Challenge/Scenes/Home/HomeVC.swift
@@ -2,6 +2,10 @@ import UIKit
 
 class HomeVC: UIViewController {
     
+    // MARK: IBOutlets
+    @IBOutlet weak var navigationBarView: UIView!
+
+    
     // MARK: Properties
     public class var storyboardName: String {
         return "Home"
@@ -12,9 +16,17 @@ class HomeVC: UIViewController {
         let viewController = storyboard.instantiateViewController(withIdentifier: String(describing: HomeVC.self)) as? HomeVC
         return viewController!
     }
-
+    
+    lazy var customNavigationBarView: UCNavigationBarView = {
+        let view = UCNavigationBarView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: self.navigationBarView.frame.size.height), title: "Sign Up")
+        return view
+    }()
+    
+    
+    // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        navigationController?.navigationBar.isHidden = true
+        navigationBarView.addSubview(customNavigationBarView)
     }
 }
