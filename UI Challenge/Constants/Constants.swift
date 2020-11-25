2ff83073d66e86b689dc88c9c8e32dd1ba9b564a
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-11-25 20:24:56 +0530

    Asserts refactored

---
+++ UI Challenge/Constants/Constants.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+struct Asserts {
+    
+    static let apps = UIImage(systemName: "square.stack.3d.up")!
+    static let appsFill = UIImage(systemName: "square.stack.3d.up.fill")!
+    
+    static let gameController = UIImage(systemName: "gamecontroller")!
+    static let gameControllerFill = UIImage(systemName: "gamecontroller.fill")!
+    
+    static let archade = UIImage(systemName: "dollarsign.circle")!
+    static let archadeFill = UIImage(systemName: "dollarsign.circle.fill")!
+
+    static let search = UIImage(systemName: "magnifyingglass")!
+}
+
+struct Fonts {
+    static let avenirNext = "Avenir Next"
+}
