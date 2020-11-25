bea49f1973f6bd3aeb8fd7faa8ef679aa4e904ce
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-11-25 21:22:07 +0530

    Section and App models added and collection view configured in AppStoreVC

--- UI Challenge/Scenes/App Store/Apps/AppStoreVC.swift
+++ UI Challenge/Scenes/App Store/Apps/AppStoreVC.swift
@@ -1,11 +1,33 @@
 import UIKit
 
 class AppStoreVC: UIViewController {
+    
+    // MARK: Properties
+    private let sections = Bundle.main.decode([Section].self, from: "appstore.json")
+    private var dataSource: UICollectionViewDiffableDataSource<Section, App>?
+    
+    private var collectionView: UICollectionView!
 
+    
+    // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+    }
+}
+
+
+// MARK: - Private Methods
+extension AppStoreVC {
+    
+    func setupViews() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         title = "Apps"
+        
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
     }
 }
